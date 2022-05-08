5e97499e1534cc1e65bb045ee05a7990fdd791e9
Bibhu Sahoo <sh.bibhu@gmail.com>
2022-05-09 01:25:38 +0530

    add maven archetype

---
+++ src/test/java/com/adobe/granite/archetypes/aem_project_archetype/AppTest.java
@@ -0,0 +1,20 @@
+package com.adobe.granite.archetypes.aem_project_archetype;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
